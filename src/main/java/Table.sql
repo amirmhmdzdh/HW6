87b1b77eed0d3b59bc8eb5b159c6112d1d2618b6
Your Name <you@example.com>
2024-01-08 11:51:36 -0800

    add command for , Create Authors and Books Table.sql

---
+++ src/main/java/Table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Authors
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    age        INTEGER
+
+
+);
+CREATE TABLE IF NOT EXISTS Books
+(
+    id               SERIAL PRIMARY KEY,
+    title            VARCHAR(255),
+    publication_year INTEGER,
+    Author_id        INTEGER,
+    FOREIGN KEY (Author_id) REFERENCES Authors (id)
+
+
+);
