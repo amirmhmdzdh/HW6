0ad8d2e6a03f764af70fdc265daf525da8415e20
Your Name <you@example.com>
2024-01-09 00:14:30 -0800

    add gette and setter method for all fields

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -15,4 +15,36 @@ public class Author {
         this.age = age;
         this.books = books;
     }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
 }
