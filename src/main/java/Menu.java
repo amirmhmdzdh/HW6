a2589779590053c3b995502a21012176dcd18d14
Your Name <you@example.com>
2024-01-09 01:23:55 -0800

    Create publicMenu in Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    public void publicMenu() {
+        System.out.println("***** WELCOME *****");
+        System.out.println("1-SAVE UP");
+        System.out.println("2-LOAD IN");
+        System.out.println("3-EXIT");
+        System.out.println("Choose your number: ");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }
