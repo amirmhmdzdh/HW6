5474e490701affa9bd1a570cbf46b99a2e8da819
Your Name <you@example.com>
2024-01-09 01:07:52 -0800

    add no args and all args construector

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -4,5 +4,12 @@ public class Book {
     private int publication_year;
     private Author author;
 
+    public Book() {
+    }
 
+    public Book(String title, int publication_year, Author author) {
+        this.title = title;
+        this.publication_year = publication_year;
+        this.author = author;
+    }
 }
