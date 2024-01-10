b3f58c532d5f85bc4a55abce9c2fe91a8a0363bb
Your Name <you@example.com>
2024-01-10 13:55:50 -0800

    Menu thats complete

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -5,6 +5,7 @@ public class Menu {
 
     private final Scanner scanner = new Scanner(System.in);
     private final AuthorService authorService = new AuthorService();
+    private final BookService bookService = new BookService();
 
     public Menu() throws SQLException {
     }
@@ -12,9 +13,12 @@ public class Menu {
     public void publicMenu() throws SQLException {
         System.out.println("***** WELCOME *****");
         System.out.println("1-SAVE UP");
-        System.out.println("2-LOAD IN");
-        System.out.println("3-DELETE");
-        System.out.println("4-EXIT");
+        System.out.println("2-SAVE BOOK");
+        System.out.println("3-LOAD IN");
+        System.out.println("4-LOAD BOOK");
+        System.out.println("5-DELETE");
+        System.out.println("6-EXIT");
+
         System.out.println("Choose your number: ");
         int number = scanner.nextInt();
         scanner.nextLine();
@@ -22,18 +26,37 @@ public class Menu {
         switch (number) {
 
             case 1 -> saveUp();
-            case 2 -> loadIn();
-            case 3 -> System.out.println("DELETE");
-            case 4 -> System.out.println("EXIT");
+            case 2 -> addbooks();
+            case 3 -> loadIn();
+            case 4 ->loadbook();
+            case 5 -> deletebook();
+            case 6 -> System.out.println("EXIT\n GoodLuck");
+
+
         }
     }
 
+
     public void saveUp() throws SQLException {
         authorService.register();
+
+    }
+
+    public void addbooks() throws SQLException {
+        bookService.addBook();
     }
 
     public void loadIn() throws SQLException {
         authorService.loadIn();
     }
+
+    public void loadbook() throws SQLException {
+        bookService.loadIn();
+
+    }
+  public void deletebook() throws SQLException {
+        bookService.deleteIn();
+  }
+
 
 }
