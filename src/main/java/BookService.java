b1d86f27f034c19ff5d183fea0c866973d5fc36b
Your Name <you@example.com>
2024-01-10 13:55:26 -0800

    BookService Class thats complete

--- src/main/java/BookService.java
+++ src/main/java/BookService.java
@@ -1,7 +1,44 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class BookService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final BookRepository bookRepository = new BookRepository();
+
+    public BookService() throws SQLException {
+    }
+
+    public void addBook() throws SQLException {
+
+        System.out.println("please enter title book: ");
+        String title = scanner.nextLine();
+
+        System.out.println("please enter publicationyear: ");
+        int publication_year = scanner.nextInt();
+        ;
+
+        Book book = new Book(title, publication_year);
+        System.out.println(book);
+
+        bookRepository.save(book);
+    }
+
+    public Book loadIn() throws SQLException {
+
+        System.out.println("please enter your id: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        Book book = bookRepository.load(id);
+        System.out.println(bookRepository.load(id));
+        return book;
+
+    }
+
+    public void deleteIn() throws SQLException {
+        System.out.println("Please enter the id of the book you want to delete: ");
+        Book book = loadIn();
+        bookRepository.delete(book);
 
 
-
-
-
+    }
 }
