f4d0aafcadad1ce13297188264141c45652ffbff
Your Name <you@example.com>
2024-01-10 02:05:08 -0800

    update Table.sql

--- src/main/java/Table.sql
+++ src/main/java/Table.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS Authors
     id         SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name  VARCHAR(255),
-    age        INTEGER,
-    book       VARCHAR(255)
+    age        INTEGER
 
 );
 CREATE TABLE IF NOT EXISTS Books
